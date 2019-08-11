30b11c96c59c0db788f72187802151b6dfcf9481
马贤飞 <simple_95@163.com>
2019-08-11 17:06:45 +0800

    bug修改

--- src/components/left-nav/index.jsx
+++ src/components/left-nav/index.jsx
@@ -14,6 +14,8 @@ class leftNav extends Component {
      * map + 递归调用
      */
     getMenuNodes = (menuList) => {
+        //得到当前请求的路由路径
+        const path = this.props.location.pathname
         return menuList.map(item => {
             if(!item.children) {
                 return (
@@ -25,6 +27,12 @@ class leftNav extends Component {
                     </Menu.Item>
                 )
             }else{
+                //查找一个与当前请求路径匹配的子Item
+                const cItem = item.children.find(cItem => cItem.key === path)
+                //如果存在，说明当前item的子列表需要打开
+                if(cItem){
+                    this.openKey = item.key
+                }
                 return (
                     <SubMenu
                         key={item.key}
@@ -41,10 +49,19 @@ class leftNav extends Component {
             }
         })
     }
-
+    //在第一次render()之前执行一次
+    //为第一个render准备数据（必须同步的）
+    UNSAFE_componentWillMount() {
+        this.menuNodes = this.getMenuNodes(menuList)
+    }
     render() {
+        // const menuNodes = this.getMenuNodes(menuList)
         //得到当前请求的路由路径
         const path = this.props.location.pathname
+
+        //得到需要打开菜单项的key
+        const openKey = this.openKey
+
         return (
             <div to='/' className='left-nav'>
                 <Link to='/' className='left-nav-header'>
@@ -56,10 +73,10 @@ class leftNav extends Component {
                     mode="inline"
                     theme="dark"
                     selectedKeys = {[path]}
-                    defaultOpenKeys = {[]}
+                    defaultOpenKeys = {[openKey]}
                     >
                     {
-                        this.getMenuNodes(menuList)
+                        this.menuNodes
                     }
                     
                 </Menu>
@@ -68,4 +85,9 @@ class leftNav extends Component {
     }
 }
 
+/**
+ * withRouter高阶组件：
+ * 包装非路由组件，返回一个新的组件
+ * 新的组件向非路由组件传递3个属性：history/location/match
+ */
 export default withRouter(leftNav)
